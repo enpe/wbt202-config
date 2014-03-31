1c2bb4803907b0109652ddc2c2384adc621c72d7
Nils Plath <enpe@posteo.de>
2014-03-31 12:01:13 +0200

    - Updated the structs in GPS.BIN.

--- wbt202_gps_details.h
+++ wbt202_gps_details.h
@@ -8,170 +8,143 @@
 
 struct payload_00
 {
-	uint8_t   unused_00[3];
-	uint8_t   gpgll_enabled;
-	uint8_t   unused_04[4];
+	uint8_t   unused_00[3];  ///< 0x00 <unused>
+	uint8_t   gpgll;         ///< 0x03 GPGLL [0=DISABLED, 1=ENABLED]
+	uint8_t   unused_04[4];  ///< 0x04 <unused>
 };
 
 struct struct_00
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_00 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_00 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 
 struct payload_01
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #1 ]
-	// - offset in file: 0x26
-	// - payload:
-	//     0x00 uint8[3]   <unused>
-	//     0x03 uint8      GPVTG [0=DISABLED, 1=ENABLED]
-	//     0x04 uint8[4]   <unused>
+	uint8_t   unused_00[3]; ///< 0x00 <unused>
+	uint8_t   gpvtg;        ///< 0x03 GPVTG [0=DISABLED, 1=ENABLED]
+	uint8_t   unused_04[4]; ///< 0x04 <unused>
 };
 
 struct struct_01
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_01 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_01 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 
 struct payload_02
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #2 ]
-	// - offset in file: 0x36
-	// - payload:
-	//     0x00 uint8[3]   <unused>
-	//     0x03 uint8      GPZDA [0=DISABLED, 1=ENABLED]
-	//     0x04 uint8[4]   <unused>
+	uint8_t   unused_00[3];  ///< 0x00 <unused>
+	uint8_t   gpzda;         ///< 0x03 GPZDA [0=DISABLED, 1=ENABLED]
+	uint8_t   unused_04[4];  ///< 0x04 <unused>
 };
 
 struct struct_02
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_02 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_02 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 
 struct payload_03
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #3 ]
-	// - offset in file: 0x46
-	// - payload:
-	//     0x00 uint8[10]  <unused>
-	//     0x0A uint8      Navigation Min. SVs [3;6]
-	//     0x0B uint8      <unused>
-	//     0x0C uint8      Navigation Signal Min. Strength [5;50 dbHz]
-	//     0x0D uint8      <unused>
-	//     0x0E uint8      Initial Fix must be 3D [0=OFF, 1=ON]
-	//     0x0F uint8[25]  <unused>
+	uint8_t   unused_00[10];          ///< 0x00 <unused>
+	uint8_t   min_visible_satellites; ///< 0x0A Navigation Min. SVs [3;6]
+	uint8_t   unused_0B;              ///< 0x0B <unused>
+	uint8_t   min_signal_strength;    ///< 0x0C Navigation Signal Min. Strength [5;50 dbHz]
+	uint8_t   unused_0D;              ///< 0x0D <unused>
+	uint8_t   initial_3d_fix;         ///< 0x0E Initial Fix must be 3D [0=OFF, 1=ON]
+	uint8_t   unused_0F[25];          ///< 0x0F <unused>
 };
 
 struct struct_03
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_03 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_03 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 
 struct payload_04
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #4 ]
-	// - offset in file: 0x76
-	// - payload:
-	//     0x00 uint8[3]   <unused>
-	//     0x03 uint8      Fix Mode [1=2D Only, 2=3D Only, 3=Auto 2D/3D]
-	//     0x04 uint32     2D Fix Altitude (stored as mm) [0;18000 (m)]
-	//     0x08 uint8[6]   <unused>
-	//     0x0E uint16     PDOP Mask, [0;1000] (stored as 10 times the GUI value
-	//                     (gets rid of comma))
-	//     0x10 uint16     TDOP Mask, [0;1000] (stored as 10 times the GUI value
-	//                     (gets rid of comma))
-	//     0x12 uint16     P Accuracy Map [0;65535 (m)]
-	//     0x14 uint16     T Accuracy Map [0;65535 (m)]
-	//     0x16 uint8[14]  <unused>
+	uint8_t   unused_00[3];    ///< 0x00 <unused>
+	uint8_t   fix_mode;        ///< 0x03 Fix Mode [1=2D Only, 2=3D Only, 3=Auto 2D/3D]
+	uint32_t  fix_altitude_2d; ///< 0x04 2D Fix Altitude (stored as mm) [0;18000 (m)]
+	uint8_t   unused_08[6];    ///< 0x08 <unused>
+	uint16_t  pdop_mask;       ///< 0x0E PDOP Mask, floor(10*[0;1000])
+	uint16_t  tdop_mask;       ///< 0x10 TDOP Mask, floor(10*[0;1000])
+	uint16_t  p_accuracy_map;  ///< 0x12 P Accuracy Map [0;65535 (m)]
+	uint16_t  t_accuracy_map;  ///< 0x14 T Accuracy Map [0;65535 (m)]
+	uint8_t   unused_16[14];   ///< 0x16 <unused>
 };
 
 struct struct_04
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_04 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_04 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 struct payload_05
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #5 ]
-	// - offset in file: 0xA2
-	// - payload:
-	//     0x00 uint32     LED Blink cycle [2;10000 ms] (stored as µs)
-	//     0x04 uint32     LED Off cycle [1;9999 ms] (stored as µs)
-	//     0x08 uint8[12]  <unused>
+	uint32_t  led_blink_cycle; ///< 0x00 LED Blink cycle [2;10000 ms] (stored as µs)
+	uint32_t  led_off_cycle;   ///< 0x04 LED Off cycle [1;9999 ms] (stored as µs)
+	uint8_t   unused_08;       ///< 0x08 <unused>
 };
 
 
 struct struct_05
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_05 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_05 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 
 struct payload_06
 {
-	// TODO MISSING IMPLEMENTATION
-	// [ struct #6 ]
-	// - offset in file: 0xBE
-	// - payload:
-	//     0x00 uint8      SBAS [0=OFF, 1=ON]
-	//     0x01 uint8[7]   <unused>
-
+	uint8_t   sbas;         ///< 0x00 SBAS [0=OFF, 1=ON]
+	uint8_t   unused_01[7]; ///< 0x01 <unused>
 };
 
 struct struct_06
 {
-	uint16_t   magic;
-	uint8_t    unused_01;
-	uint8_t    unused_02;
-	uint16_t   length;
-	payload_06 payload;
-	uint8_t    checksum_01;
-	uint8_t    checksum_02;
+	uint16_t   magic;       ///< 0x00    uint16    magic header value 0x62B5
+	uint8_t    unused_01;   ///< 0x02    uint8     <unused>, must be 0 < x < 14
+	uint8_t    unused_02;   ///< 0x03    uint8     <unused>
+	uint16_t   length;      ///< 0x04    uint16    N: payload length in bytes
+	payload_06 payload;     ///< 0x06    uint8[N]  payload
+	uint8_t    checksum_01; ///< 0x06+N  uint8     checksum byte 1
+	uint8_t    checksum_02; ///< 0x07+N  uint8     checksum byte 2
 };
 
 #endif // WBT202_GPS_DETAILS_H
