ed474541f74eb573549273cd852f53df6372ac17
Alexander Nitsch <nitsch@ht.tu-berlin.de>
2014-03-31 12:48:05 +0200

    - Fixed documentation that was obviously missed during previous merge and thus reverted back to an earlier state.

--- wbt202_sys.h
+++ wbt202_sys.h
@@ -6,35 +6,38 @@
 // - values are stored in little-endian byte order inside the file (least significant byte first)
 // - values in these notes are given most significant byte first
 //
-// Start	End 	Type	Name
-// 0x00		0x03	uint32	magic number 0x11111111
-// 0x04		0x17	uchar[]	Device name (max. 19 bytes + terminating NULL byte)
-// 0x18		0x2b	uchar[]	Device info (max. 19 bytes + terminating NULL byte)
-// 0x2c		0x2c	uint8	GPS Re-Start [1=Cold Start, 2=Warm Start, 3=Hot Start [bug in v4.7]]
-// 0x2d		0x2e	uint16	CID
-// 0x2f		0x30	uint16	PID (must be 0xB202)
-// 0x31		0x34	uint32	UNKNOWN, seems to be used only in export to TimeMachineX's TK2 format
-// 0x35		0x36	uint16  displayed in WBT_Tool's window title as "FW_Ver:%d.0"
-// 0x37		0x37	uchar	Shake Mode for Power Saving [0=OFF;1=ON]
-// 0x38		0x38			UNKNOWN
-// 0x39		0x39	uint8	UNKNOWN
-// 0x3a		0x3b	uint16	Shake Mode for Power Saving, timeout [60;7200 (s)]
-// 0x3c		0x3d	uint16	Bluetooth/Device auto power off setting [60;3600 (s)], 0 = OFF
-// 0x3e		0x41			UNKNOWN
-// 0x42		0x45	uint32	Password, stored as 152 * ( P + 11977 ) or as 0xFFFFFFFF if left empty
-// 0x46		0x63	uchar[]	UNKNOWN, absolute path to some .TES track file
-// 0x64		0x8b			UNKNOWN
-// 0x8c		0x8c	uint8	always set to 1 by WBT_Tool prior to writing the SYS.BIN file
-//                          Maybe a dirty flag to indicate device restart?
-// 0x8d		0x8e	int16	Reference time zone, 100 * ( difference in hours ): e.g. -150 = -1.5h
-// 0x8f		0x8f	uint8	Language selected in WBT_Tool
-//                                0=English,
-//                                1=Traditional Chinese
-//                                2=Simplified Chinese
-//                                3=German
-//                                4=Japanese
-// 0x90		0x90	uchar	Reference unit [0=Metric,1=Imperial]
-// 0x91		0x94	uint32	magic number 0x22222222
+// Start    Type	    Name/Description
+// 0x00     uint32      magic number 0x11111111
+// 0x04     uchar[]     Device name (max. 19 bytes + terminating NULL byte)
+// 0x18     uchar[]     Device info (max. 19 bytes + terminating NULL byte)
+// 0x2c     uint8       GPS Re-Start
+//                          1=Cold Start
+//                          2=Warm Start
+//                          3=Hot Start (bug in WBT_Tool 4.7 will always set this to 2)
+// 0x2d     uint16      CID
+// 0x2f     uint16      PID (must be 0xB202)
+// 0x31     uint32      UNKNOWN, seems to be used only in export to TimeMachineX's TK2 format
+// 0x35     uint16      displayed in WBT_Tool's window title as "FW_Ver:%d.0"
+// 0x37     uint8       Shake Mode for Power Saving [0=OFF;1=ON]
+// 0x38                 UNKNOWN
+// 0x39     uint8       UNKNOWN
+// 0x3a     uint16      Shake Mode for Power Saving, timeout [60;7200 (s)]
+// 0x3c     uint16      Bluetooth/Device auto power off setting [60;3600 (s)], 0 = OFF
+// 0x3e                 UNKNOWN
+// 0x42     uint32      Password, stored as 152 * ( P + 11977 ) or as 0xFFFFFFFF if left empty
+// 0x46     uchar[]     UNKNOWN, absolute path to some .TES track file
+// 0x64                 UNKNOWN
+// 0x8c     uint8       always set to 1 by WBT_Tool prior to writing the SYS.BIN file
+//                        Maybe a dirty flag to indicate device restart?
+// 0x8d     int16       Reference time zone, 100 * ( difference in hours ): e.g. -150 = -1.5h
+// 0x8f     uint8       Language selected in WBT_Tool
+//                          0=English
+//                          1=Traditional Chinese
+//                          2=Simplified Chinese
+//                          3=German
+//                          4=Japanese
+// 0x90     uint8       Reference unit [0=Metric,1=Imperial]
+// 0x91     uint32      magic number 0x22222222
 
 #include <stdint.h>
 
