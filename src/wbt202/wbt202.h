ae09efb70517934a374e7f114102d5776ce20c04
Nils Plath <enpe@posteo.de>
2014-04-04 17:19:49 +0200

    - Removed define (since it will be defined in the project file anyway).

--- src/wbt202/wbt202.h
+++ src/wbt202/wbt202.h
@@ -3,11 +3,6 @@
 #ifndef WBT202_H
 #define WBT202_H
 
-// By default, assume hardware uses little endian byte order.
-#ifndef IS_BIG_ENDIAN
-#define IS_BIG_ENDIAN 0
-#endif // IS_BIG_ENDIAN
-
 #include <wbt202/GpsBin.h>
 #include <wbt202/LogBin.h>
 #include <wbt202/SysBin.h>
